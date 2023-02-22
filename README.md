df717e34855ec57e10c0b66c61723075e143bf48
Paweł 'felixd' Wojciechowski <felixd@konopnickiej.com>
2023-02-22 20:52:07 +0100

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# MicroPython compillation for W5100S_EVB_Pico
+
+Full MicroPython UF2 file for RP2040
+
+## KB
+
+* https://docs.wiznet.io/Product/iEthernet#wiznet-iolibrary-examples
+
+* https://docs.wiznet.io/Product/iEthernet/W5100S/w5100s-evb-pico
+* https://forum.micropython.org/viewtopic.php?f=21&t=12955
+* https://github.com/Wiznet/RP2040-HAT-MicroPython/tree/main/examples/MQTT/Subscribe
+
+* [mpremote](https://docs.micropython.org/en/latest/reference/mpremote.html#mpremote)
+
+## Author
+
+* Paweł 'felixd' Wojciechowski - [FlameIT - Immersion Cooling](https://flameit.io)
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Installing MicroPython for W5100S EVB board"
+
+FIRMWARE_FILENAME=firmware-rp2-W5100S-EVB-Pico-$(date +"%Y-%m-%d").uf2
+
+if [[ -d "micropython" ]]
+then
+    echo "MicroPython exists on your filesystem"
+    cd micropython
+    git pull
+    cd ..
+else
+    echo "Clonning MicroPython"
+    git clone -b master https://github.com/micropython/micropython.git
+fi
+
+cd micropython
+make -C ports/rp2 submodules
+make -C ports/rp2 BOARD=W5100S_EVB_PICO submodules
+make -C mpy-cross
+cd ports/rp2
+make BOARD=W5100S_EVB_PICO submodules
+make BOARD=W5100S_EVB_PICO
+
+cd ../../../
+echo "Compiling finished"
+
+
+cp micropython/ports/rp2/build-W5100S_EVB_PICO/firmware.uf2 ./$FIRMWARE_FILENAME
